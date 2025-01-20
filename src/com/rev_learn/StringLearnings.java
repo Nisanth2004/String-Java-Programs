0a2d32293255e226ea731698f47a8559aff68593
Nisanth2004 <nisanthjothi2004@gmail.com>
2025-01-20 07:46:22 +0530

    Initial commit

---
+++ src/com/rev_learn/StringLearnings.java
@@ -0,0 +1,12 @@
+package com.rev_learn;
+
+public class StringLearnings {
+    public static void main(String[] args) {
+        String s1=new String("Muthu");
+        String s2="Muthu"; // literal
+        System.out.println(s1.length());
+
+        System.out.println(s1.isBlank());
+        System.out.println(s1.isEmpty());
+    }
+}
